0d2194eb9713d3f3b086432c759c35d9c07fbe73
KIMHYEONGJIN5925 <93527060+KIMHYEONGJIN5925@users.noreply.github.com>
2022-04-14 00:21:12 +0900

    login

--- client/src/_components/views/LandingPage/LandingPage.js
+++ client/src/_components/views/LandingPage/LandingPage.js
@@ -6,7 +6,19 @@ function LandingPage() {
       console.log(response);
     });
   }, []);
-  return <div>LandingPage 입니다zz.</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <h2>시작 페이지</h2>
+    </div>
+  );
 }
 
 export default LandingPage;
